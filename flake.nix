7e621ca31ad4411fe7842e620000ed4a475bcaaf
francocalvo <dev@francocalvo.ar>
2024-02-21 19:25:43 -0300

    create a dev shell for the vm

--- flake.nix
+++ flake.nix
@@ -64,44 +64,81 @@
           zc-images-m1 = zc-images-m1;
         };
 
-        devShells.default = pkgs.mkShell {
-          nativeBuildInputs = [ python ] ++ (with python.pkgs; [
-            # Python dependencies
-            black
-            pip
-            pytest
-            pytest-cov
-          ]) ++ (with pkgs; [
-            # Dev dependencies
-            engage
-            nixpkgs-fmt
-            poetry
-            ruff
-            docker
-            cargo
-            wget
-            lunarvim
+        devShells = {
+          default = pkgs.mkShell {
+            nativeBuildInputs = [ python ] ++ (with python.pkgs; [
+              # Python dependencies
+              black
+              pip
+              pytest
+              pytest-cov
+            ]) ++ (with pkgs; [
+              # Dev dependencies
+              engage
+              nixpkgs-fmt
+              poetry
+              ruff
+              docker
+              cargo
+              wget
 
-            # Zoomcamp dependencies
-            libpqxx
-            postgresql
-            terraform
-            docker
-            google-cloud-sdk
-          ]) ++ (with pkgs.nodePackages;
-            [ # NodeJS dependencies
-              markdownlint-cli
-            ]);
+              # Zoomcamp dependencies
+              libpqxx
+              postgresql
+              terraform
+              docker
+              google-cloud-sdk
+            ]) ++ (with pkgs.nodePackages;
+              [ # NodeJS dependencies
+                markdownlint-cli
+              ]);
 
-          NIX_PYTHON_SITE_PACKAGES = python.sitePackages;
+            NIX_PYTHON_SITE_PACKAGES = python.sitePackages;
 
-          shellHook = ''
-            export LD_LIBRARY_PATH="${
-              pkgs.lib.makeLibraryPath [ pkgs.zlib ]
-            }:$LD_LIBRARY_PATH"
+            shellHook = ''
+              export LD_LIBRARY_PATH="${
+                pkgs.lib.makeLibraryPath [ pkgs.zlib ]
+              }:$LD_LIBRARY_PATH"
 
-            export LD_LIBRARY_PATH="${pkgs.stdenv.cc.cc.lib}/lib:$LD_LIBRARY_PATH"
-          '';
+              export LD_LIBRARY_PATH="${pkgs.stdenv.cc.cc.lib}/lib:$LD_LIBRARY_PATH"
+            '';
+          };
+
+          vm = pkgs.mkShell {
+            nativeBuildInputs = [ python ] ++ (with python.pkgs; [
+              # Python dependencies
+              black
+              pip
+              pytest
+              pytest-cov
+            ]) ++ (with pkgs; [
+              # Dev dependencies
+              poetry
+              ruff
+              docker
+              cargo
+              wget
+
+              # Zoomcamp dependencies
+              libpqxx
+              postgresql
+              docker
+            ]) ++ (with pkgs.nodePackages;
+              [ # NodeJS dependencies
+                markdownlint-cli
+              ]);
+
+            NIX_PYTHON_SITE_PACKAGES = python.sitePackages;
+
+            shellHook = ''
+              export LD_LIBRARY_PATH="${
+                pkgs.lib.makeLibraryPath [ pkgs.zlib ]
+              }:$LD_LIBRARY_PATH"
+
+              export LD_LIBRARY_PATH="${pkgs.stdenv.cc.cc.lib}/lib:$LD_LIBRARY_PATH"
+            '';
+          };
+
         };
 
         checks = {
