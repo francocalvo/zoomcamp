2e9d0f09b2aec86452396565dc575f5a300cc818
francocalvo <dev@francocalvo.ar>
2024-02-27 11:35:38 -0300

    update dependencies and delete unused files

--- src/m1_module/terraform/configuration.nix
+++
@@ -1,9 +0,0 @@
-{ modulesPath, ... }: {
-  imports =
-    [ "${toString modulesPath}/virtualisation/google-compute-image.nix" ];
-
-  users.users.root.openssh.authorizedKeys.keyFiles =
-    [ (/. + builtins.getEnv ("HOME") + "/.ssh/id_rsa.pub") ];
-
-  system.stateVersion = "23.05";
-}
